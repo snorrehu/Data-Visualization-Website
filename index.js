84771a278659c3f575772ed271c93fd8b49e9ee3
fbjerko <f.bjerko@gmail.com>
2018-09-26 10:26:38 +0200

    Node server up, added express.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-	response.write("Hello, world" + new Date());
-	response.end();
-}).listen(process.env.PORT || 5000);
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use( express.static(__dirname + '/web'));
+
+app
+  .get( '/', function( req, res ) {
+    res.sendFile( path.join( __dirname, 'web', 'index.html' ));
+  });
+app.listen(8080);
