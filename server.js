ee62d2eec20ce858351737e3a2bcedfbf3e2b610
fbjerko <f.bjerko@gmail.com>
2018-09-26 10:32:07 +0200

    fixed structure

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use( express.static(__dirname + '/web'));
+app.use( express.static(__dirname + '/public'));
+app.use( express.static(__dirname + '/views'));
 
 app
   .get( '/', function( req, res ) {
-    res.sendFile( path.join( __dirname, 'web', 'index.html' ));
+    res.sendFile( path.join( __dirname, 'views', 'index.html' ));
   });
 app.listen(8080);
