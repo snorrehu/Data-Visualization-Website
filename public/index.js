ba7463dd7b2d942571cab392d8bfcf9a712f5f29
fbjerko <f.bjerko@gmail.com>
2018-09-28 12:46:43 +0200

    ads

--- public/index.js
+++ public/index.js
@@ -139,3 +139,42 @@ d3.csv("iris.csv", function (flowers) {
         return c;
     }
 });
+
+//because read.csv is in utils
+ocpu.seturl("//public.opencpu.org/ocpu/library/utils/R");
+
+//actual handler
+$("#submitbutton").on("click", function(){
+
+    //arguments
+    var myheader = $("#header").val() == "true";
+    var myfile = $("#csvfile")[0].files[0];
+        
+    if(!myfile){
+        alert("No file selected.");
+        return;
+    }
+
+    //disable the button during upload
+    $("#submitbutton").attr("disabled", "disabled");
+
+    //perform the request
+    var req = ocpu.call("read.csv", {
+        "file" : myfile,
+        "header" : myheader
+    }, function(session){
+        session.getConsole(function(outtxt){
+            $("#output").text(outtxt); 
+        });
+    });
+        
+    //if R returns an error, alert the error message
+    req.fail(function(){
+        alert("Server error: " + req.responseText);
+    });
+    
+    //after request complete, re-enable the button 
+    req.always(function(){
+        $("#submitbutton").removeAttr("disabled")
+    });        
+});    
