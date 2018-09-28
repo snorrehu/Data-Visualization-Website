3387f70360db1c54887076e385e5686c94f3c03e
fbjerko <f.bjerko@gmail.com>
2018-09-28 17:09:15 +0200

    fixed

--- public/index.js
+++ public/index.js
@@ -1,4 +1,29 @@
-d3.csv('iris.csv',function (data) {
+var fileInput = document.getElementById("csv");
+let csvInput;
+
+    readFile = function () {
+        var reader = new FileReader();
+        reader.onload = function () {
+           
+          csvCall(reader.result);
+        };
+        // start reading the file. When it is done, calls the onload event defined above.
+        reader.readAsDataURL(fileInput.files[0]);
+        
+    };
+
+fileInput.addEventListener('change', readFile); 
+
+function getByUrl(event) {
+    event.preventDefault();
+    csvInput = document.getElementById("csvUrl").value;
+    csvCall(csvInput);
+}
+
+
+function csvCall(csvInput) {
+
+d3.csv(csvInput,function (data) {
 // CSV section
     var body = d3.select('body')
     var headers = d3.keys(data[0]);
@@ -175,3 +200,4 @@ d3.csv('iris.csv',function (data) {
             .attr('cx',function (d) { return xScale(d[value]) })
     }
 })
+}
