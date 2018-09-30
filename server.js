3930c36808d28eb096272275e9c69cf7e908cb64
fbjerko <f.bjerko@gmail.com>
2018-09-30 18:38:34 +0200

    daijsdas

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use( express.static(__dirname + '/public'));
-app.use( express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
-app
-  .get( '/', function( req, res ) {
-    res.sendFile( path.join( __dirname, 'views', 'index.html' ));
-    res.sendFile( path.join( __dirname, 'public' ));
-  });
-app.listen(8888);
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+	res.sendFile(path.join(__dirname, 'public'));
+});
+app.listen(process.env.PORT || 8888);
